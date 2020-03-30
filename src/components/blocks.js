b5a14e3c92d959a4f901acb4100386560aa3ac9a
kingofdevs <40970351+kingofdevs@users.noreply.github.com>
2020-03-30 17:57:49 +0800

    Update blocks.js

--- src/components/blocks.js
+++ src/components/blocks.js
@@ -1,6 +1,5 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import React from 'react'
 import YouTubePlayer from 'react-player/lib/players/YouTube'
 
 export const Container = ({ wide, ...props }) => (
